d8202eb969cdb03ddacdaae490b351b792bb6c6a
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-12 11:46:16 +0300

    Deleted the likes and dislikes declaratives in the quote class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,13 +1,13 @@
 export class Quote {
       showQuote: boolean;
+  completeQuote: Date;
+  completeDate: string | number | Date;
       constructor(
             public id: number, 
             public name: string, 
             public quote: string,
             public author: string,
-            public postedDate: Date,
-            public likes: number,
-            public dislikes: number
+            public postedDate: Date
             ) {
             this.showQuote = false;
       }
