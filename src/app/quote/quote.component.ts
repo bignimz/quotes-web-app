8169d240b8410a5a14b0592e57d010c44a6e9448
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-12 10:03:54 +0300

    Added styling to the quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,6 +12,38 @@ export class QuoteComponent implements OnInit {
   faThumbsUp = faThumbsUp;
   faThumbsDown = faThumbsDown;
 
+  quotes = [
+    {
+      id: 1,
+      title: 'Quote 1',
+      description: 'Description one',
+      author: 'John Doe',
+      
+      date: '10.04.2022'
+    },
+    {
+      id: 2,
+      title: 'Quote 2',
+      description: 'Description one',
+      author: 'John Doe',
+      date: '10.04.2022'
+    },
+    {
+      id: 3,
+      title: 'Quote 3',
+      description: 'Description one',
+      author: 'John Doe',
+      date: '10.04.2022'
+    },
+    {
+      id: 4,
+      title: 'Quote 4',
+      description: 'Description one',
+      author: 'John Doe',
+      date: '10.04.2022'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
