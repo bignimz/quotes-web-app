02af6d746681347a6319d9858af37ce9c736373e
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-12 10:29:20 +0300

    Added a for loop to display the array of quotes in the quote component html

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -23,28 +23,31 @@ export class QuoteComponent implements OnInit {
       title: 'Quote 1',
       description: 'Description one',
       author: 'John Doe',
-
+      name: 'Emily Short',
       date: '10.04.2022'
     },
     {
       id: 2,
       title: 'Quote 2',
-      description: 'Description one',
+      description: 'Description Two',
       author: 'John Doe',
-      date: '10.04.2022'
+      date: '10.04.2022',
+      name: 'Tim Wang'
     },
     {
       id: 3,
       title: 'Quote 3',
-      description: 'Description one',
+      description: 'Description Three',
       author: 'John Doe',
-      date: '10.04.2022'
+      date: '10.04.2022',
+      name: 'Peter Mosh'
     },
     {
       id: 4,
       title: 'Quote 4',
-      description: 'Description one',
+      description: 'Description Four',
       author: 'John Doe',
+      name: 'Mike Epps',
       date: '10.04.2022'
     }
   ];
